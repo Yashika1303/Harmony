4fa62b98f6efa25ee8ee4ade706d4eadda6501e3
Yashika mishra <yashikashrutimishra@gmail.com>
2025-03-24 17:08:13 +0530

    change the github URl

--- harmony-bookings/src/components/Footer.jsx
+++ harmony-bookings/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
     icon: <AiFillYoutube className='group-hover:text-white w-4 h-5 ' />
   },
   {
-    path: "https://github.com/Venerablevivek",
+    path: "https://github.com/Yashika1303",
     icon: <AiFillGithub className='group-hover:text-white w-4 h-5 ' />
   }
   ,{
